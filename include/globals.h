6478f91e4d77e5fe81082771317e810545110ef9
danya2271 <dany2271@yandex.ru>
2025-10-15 20:40:12 +0500

    20 tps

--- include/globals.h
+++ include/globals.h
@@ -35,7 +35,7 @@
 #define VIEW_DISTANCE 32
 
 // Time between server ticks in microseconds (default = 1s)
-#define TIME_BETWEEN_TICKS 1000000
+#define TIME_BETWEEN_TICKS 50000
 
 // Calculated from TIME_BETWEEN_TICKS
 #define TICKS_PER_SECOND ((float)1000000 / TIME_BETWEEN_TICKS)
